3972e3f6f019e7b8b62bc1aa5c693905a98f371f
fgiering <fgiering@web.de>
2022-09-19 22:41:28 +0200

    a few changes

--- public/watch.js
+++ public/watch.js
@@ -1,47 +1,43 @@
 let peerConnection;
 const config = {
   iceServers: [
-      { 
-        "urls": "stun:stun.l.google.com:19302",
-      },
-      // { 
-      //   "urls": "turn:TURN_IP?transport=tcp",
-      //   "username": "TURN_USERNAME",
-      //   "credential": "TURN_CREDENTIALS"
-      // }
-  ]
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    // {
+    //   "urls": "turn:TURN_IP?transport=tcp",
+    //   "username": "TURN_USERNAME",
+    //   "credential": "TURN_CREDENTIALS"
+    // }
+  ],
 };
 
 const socket = io.connect(window.location.origin);
-const video = document.querySelector("video");
-const enableAudioButton = document.querySelector("#enable-audio");
-
-enableAudioButton.addEventListener("click", enableAudio)
+const audio = document.querySelector("audio");
 
 socket.on("offer", (id, description) => {
   peerConnection = new RTCPeerConnection(config);
   peerConnection
     .setRemoteDescription(description)
     .then(() => peerConnection.createAnswer())
-    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then((sdp) => peerConnection.setLocalDescription(sdp))
     .then(() => {
       socket.emit("answer", id, peerConnection.localDescription);
     });
-  peerConnection.ontrack = event => {
-    video.srcObject = event.streams[0];
+  peerConnection.ontrack = (event) => {
+    audio.srcObject = event.streams[0];
   };
-  peerConnection.onicecandidate = event => {
+  peerConnection.onicecandidate = (event) => {
     if (event.candidate) {
       socket.emit("candidate", id, event.candidate);
     }
   };
 });
 
-
 socket.on("candidate", (id, candidate) => {
   peerConnection
     .addIceCandidate(new RTCIceCandidate(candidate))
-    .catch(e => console.error(e));
+    .catch((e) => console.error(e));
 });
 
 socket.on("connect", () => {
@@ -56,8 +52,3 @@ window.onunload = window.onbeforeunload = () => {
   socket.close();
   peerConnection.close();
 };
-
-function enableAudio() {
-  console.log("Enabling audio")
-  video.muted = false;
-}
